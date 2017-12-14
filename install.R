fc14b93133c3949df3e22099aadf0abba6d9dac2
Simon Larsen <simonhffh@gmail.com>
2017-12-14 13:24:30 +0100

    Added Dockerfile and docker install script

---
+++ install.R
@@ -0,0 +1,14 @@
+install.packages(c('devtools','shiny', 'shinythemes', 'visNetwork', 'data.table', 'igraph', 'magrittr', 'circlize', 'ggplot2', 'caret', 'mccr'))
+
+source('https://bioconductor.org/biocLite.R')
+biocLite('qvalue', ask=FALSE)
+biocLite('ComplexHeatmap', ask=FALSE)
+biocLite('org.Hs.eg.db', ask=FALSE)
+biocLite('DOSE', ask=FALSE)
+biocLite('clusterProfiler', ask=FALSE)
+
+devtools::install_github('AnalytixWare/ShinySky')
+devtools::install_github('SimonLarsen/grandforest', auth_token='edb93ddf877c24e3e71a66fdb96092c2e96bed82')
+
+#devtools::install_github('gaborcsardi/pkgconfig')
+#devtools::install_github('igraph/rigraph')
